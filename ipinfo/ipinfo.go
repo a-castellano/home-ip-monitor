ed965f8b23f45bc34bdd900eb632cc4416bb5a20
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2024-07-06 17:18:24 +0200

    IPinfo management

---
+++ ipinfo/ipinfo.go
@@ -0,0 +1,74 @@
+package ipinfo
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// IPinfo stores ingo retrieved from https://ipinfo.io/ calls
+type IPinfo struct {
+	IP       string `json:"ip"`
+	Hostname string `json:"hostname"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
+	Timezone string `json:"timezone"`
+	Readme   string `json:"readme"`
+	OrgName  string
+}
+
+// getOrgName retrieves OrgName from Org field
+func (ipInfo *IPinfo) getOrgName() {
+	ipInfo.OrgName = strings.Split(ipInfo.Org, " ")[1]
+}
+
+// Requester interface must implement GetIPInfo method
+type Requester interface {
+	GetIPInfoResponse() (*http.Response, error)
+}
+
+// RetireveIPInfoFromResponse Proceses GetIPInfoResponse response and return IPinfo data
+func RetireveIPInfoFromResponse(requester Requester) (IPinfo, error) {
+
+	var retrievedInfo IPinfo
+	response, responseError := requester.GetIPInfoResponse()
+
+	if responseError != nil {
+		return retrievedInfo, responseError
+	}
+
+	defer response.Body.Close()
+	bs, _ := ioutil.ReadAll(response.Body)
+
+	unmarshalErr := json.Unmarshal(bs, &retrievedInfo)
+	if unmarshalErr != nil {
+		return retrievedInfo, unmarshalErr
+	}
+
+	if retrievedInfo.IP == "" {
+		return retrievedInfo, errors.New("no IPInfo was found during request phase")
+	}
+	retrievedInfo.getOrgName()
+
+	return retrievedInfo, nil
+
+}
+
+// Realrequester is the actual requester
+type Realrequester struct {
+	Client http.Client
+}
+
+// GetIPInfoResponse retrievesa ctual ipinfo response
+func (requester Realrequester) GetIPInfoResponse() (*http.Response, error) {
+	request, _ := http.NewRequest("GET", "https://ipinfo.io/", nil)
+	response, responseError := requester.Client.Do(request)
+
+	return response, responseError
+}
---
+++ ipinfo/ipinfo_test.go
@@ -0,0 +1,111 @@
+//go:build integration_tests || unit_tests || ipinfo_tests || ipinfo_unit_tests
+
+package ipinfo
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+type RoundTripperMock struct {
+	Response *http.Response
+	RespErr  error
+}
+
+func (rtm *RoundTripperMock) RoundTrip(*http.Request) (*http.Response, error) {
+	return rtm.Response, rtm.RespErr
+}
+
+type MockIPinfo struct {
+	provider string
+}
+
+func (m MockIPinfo) GetIPInfoResponse() (*http.Response, error) {
+
+	var client http.Client
+
+	switch m.provider {
+
+	case "Digi":
+		client = http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`{"ip": "79.12.12.12","hostname": "79-12-12-12.digimobil.es","city": "Madrid","region": "Madrid","country": "ES","loc": "40.4165,-3.7026","org": "AS57269 DIGI SPAIN TELECOM S.L.","postal": "28087","timezone": "Europe/Madrid","readme": "https://ipinfo.io/missingauth"}`))}}}
+
+	case "Telefonica":
+		client = http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`{"ip": "95.12.12.12","hostname": "12.red-95-12-12.staticip.rima-tde.net","city": "Valencia","region": "Valencia","country": "ES","loc": "39.4739,-0.3797","org": "AS3352 TELEFONICA DE ESPANA S.A.U.","postal": "46001","timezone": "Europe/Madrid","readme": "https://ipinfo.io/missingauth"}`))}}}
+
+	case "invalid":
+		client = http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`{"nonsense": "json"}`))}}}
+
+	default:
+		client = http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`"nonsense": "json"}`))}}}
+	}
+
+	request, _ := http.NewRequest("GET", "https://ipinfo.io/", nil)
+	response, responseError := client.Do(request)
+
+	return response, responseError
+}
+
+func TestErroredRequester(t *testing.T) {
+
+	erroredRequester := MockIPinfo{}
+	_, ipInfoErr := RetireveIPInfoFromResponse(erroredRequester)
+
+	if ipInfoErr == nil {
+		t.Errorf("TestErroredRequester should fail")
+	} else {
+		if ipInfoErr.Error() != "invalid character ':' after top-level value" {
+			t.Errorf("TestErroredRequester error should be \"invalid character ':' after top-level value\", not \"%s\".", ipInfoErr.Error())
+		}
+	}
+
+}
+
+func TestInvalidRequester(t *testing.T) {
+
+	invalidRequester := MockIPinfo{provider: "invalid"}
+	_, ipInfoErr := RetireveIPInfoFromResponse(invalidRequester)
+
+	if ipInfoErr == nil {
+		t.Errorf("TestInvalidRequester should fail.")
+	} else {
+		if ipInfoErr.Error() != "no IPInfo was found during request phase" {
+			t.Errorf("TestInvalidRequester error should be \"no IPInfo was found during request phase\", not \"%s\".", ipInfoErr.Error())
+		}
+	}
+}
+
+func TestDigiRequester(t *testing.T) {
+
+	digiRequester := MockIPinfo{provider: "Digi"}
+	ipInfo, ipInfoErr := RetireveIPInfoFromResponse(digiRequester)
+
+	if ipInfoErr != nil {
+		t.Errorf("TestDigiRequester should not fail, error was \"%s\".", ipInfoErr.Error())
+	} else {
+		if ipInfo.IP != "79.12.12.12" {
+			t.Errorf("ipInfo.IP should be \"79.12.12.12\" not \"%s\".", ipInfo.IP)
+		}
+		if ipInfo.OrgName != "DIGI" {
+			t.Errorf("ipInfo.OrgName should be \"DIGI\" not \"%s\".", ipInfo.OrgName)
+		}
+	}
+}
+
+func TestTelefonicaRequester(t *testing.T) {
+
+	telefonicaRequester := MockIPinfo{provider: "Telefonica"}
+	ipInfo, ipInfoErr := RetireveIPInfoFromResponse(telefonicaRequester)
+
+	if ipInfoErr != nil {
+		t.Errorf("TesttelefonicaRequester should not fail, error was \"%s\".", ipInfoErr.Error())
+	} else {
+		if ipInfo.IP != "95.12.12.12" {
+			t.Errorf("ipInfo.IP should be \"95.12.12.12\" not \"%s\".", ipInfo.IP)
+		}
+		if ipInfo.OrgName != "TELEFONICA" {
+			t.Errorf("ipInfo.OrgName should be \"TELEFONICA\" not \"%s\".", ipInfo.OrgName)
+		}
+	}
+}
