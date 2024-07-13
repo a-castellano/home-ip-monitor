e81a443133ae596ed839fbdf7dc0bcd5eae1cbdd
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2024-07-13 20:53:39 +0000

    Write main.go

--- ipinfo/ipinfo.go
+++ ipinfo/ipinfo.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -26,6 +27,7 @@ type IPinfo struct {
 // getOrgName retrieves OrgName from Org field
 func (ipInfo *IPinfo) getOrgName() {
 	ipInfo.OrgName = strings.Split(ipInfo.Org, " ")[1]
+	log.Printf("Retrieved OrgName is \"%s\"", ipInfo.OrgName)
 }
 
 // Requester interface must implement GetIPInfo method
@@ -37,6 +39,7 @@ type Requester interface {
 func RetireveIPInfoFromResponse(requester Requester) (IPinfo, error) {
 
 	var retrievedInfo IPinfo
+
 	response, responseError := requester.GetIPInfoResponse()
 
 	if responseError != nil {
@@ -54,6 +57,8 @@ func RetireveIPInfoFromResponse(requester Requester) (IPinfo, error) {
 	if retrievedInfo.IP == "" {
 		return retrievedInfo, errors.New("no IPInfo was found during request phase")
 	}
+
+	log.Printf("Retrieved IP is \"%s\"", retrievedInfo.IP)
 	retrievedInfo.getOrgName()
 
 	return retrievedInfo, nil
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/syslog"
+	"net/http"
+	"os"
+	"time"
+
+	memorydatabase "github.com/a-castellano/go-services/memorydatabase"
+	"github.com/a-castellano/go-services/messagebroker"
+	config "github.com/a-castellano/home-ip-monitor/config"
+	"github.com/a-castellano/home-ip-monitor/ipinfo"
+	"github.com/a-castellano/home-ip-monitor/monitor"
+)
+
+func main() {
+
+	// Configure logger to write to the syslog. You could do this in init(), too.
+	logwriter, e := syslog.New(syslog.LOG_INFO, "home-ip-monitor")
+	if e == nil {
+		log.SetOutput(logwriter)
+		// Remove timestamp
+		log.SetFlags(0)
+	}
+
+	// Now from anywhere else in your program, you can use this:
+	log.Print("Loading config")
+
+	appConfig, configErr := config.NewConfig()
+
+	if configErr != nil {
+		log.Print(configErr.Error())
+		os.Exit(1)
+	}
+
+	log.Print("Defining http client used by ipInfo package")
+
+	httpClient := http.Client{
+		Timeout: time.Second * 5, // Maximum of 5 secs
+	}
+
+	ctx := context.Background()
+
+	requester := ipinfo.Realrequester{Client: httpClient}
+
+	log.Print("Creating Redis client")
+	redisClient := memorydatabase.NewRedisClient(appConfig.RedisConfig)
+	log.Print("Initiating Redis client")
+	redisClientError := redisClient.Initiate(ctx)
+	if redisClientError != nil {
+		log.Print(redisClientError.Error())
+		os.Exit(1)
+	}
+	memoryDatabase := memorydatabase.NewMemoryDatabase(&redisClient)
+
+	log.Print("Creating RabbitMQ client")
+	rabbitmqClient := messagebroker.NewRabbimqClient(appConfig.RabbitmqConfig)
+	messageBroker := messagebroker.MessageBroker{Client: rabbitmqClient}
+
+	monitorError := monitor.Monitor(ctx, requester, memoryDatabase, messageBroker, appConfig)
+	if monitorError != nil {
+		log.Print(monitorError.Error())
+		os.Exit(1)
+	}
+
+	log.Print("Execution finished")
+}
