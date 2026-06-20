3f221760683a93a736da8402d5108247006b37f3
Álvaro Castellano <alvaro@windmaker.net>
2026-06-21 01:04:52 +0200

    Finish refactor of internal/infra/ipinfodata

--- internal/infra/ipinfodata/ipinfodata_integration_test.go
+++ internal/infra/ipinfodata/ipinfodata_integration_test.go
@@ -1,30 +1,29 @@
-//go:build integration_tests || ipinfodata_tests
+//go:build integration_tests || unit_tests || ipinfodata_tests || ipinfodata_integration_tests
 
 package ipinfodata
 
-//import (
-//	"net/http"
-//	"testing"
-//	"time"
-//)
+import (
+	"contex"
+	"net/http"
+	"testing"
+	"time"
+)
 
-//func TestIPInfoRequester(t *testing.T) {
-//
-//	client := http.Client{
-//		Timeout: time.Second * 5, // Maximum of 5 secs
-//	}
-//
-//	requester := Realrequester{Client: client}
-//	ipInfo, ipInfoErr := RetrieveIPInfoFromResponse(requester)
-//
-//	if ipInfoErr != nil {
-//		t.Errorf("TesttelefonicaRequester should not fail, error was \"%s\".", ipInfoErr.Error())
-//	} else {
-//		if ipInfo.IP == "" {
-//			t.Errorf("ipInfo.IP should not be empty.")
-//		}
-//		if ipInfo.OrgName == "" {
-//			t.Errorf("ipInfo.OrgName should not be empty.")
-//		}
-//	}
-//}
+func TestIPInfoRequester(t *testing.T) {
+
+	client := http.Client{
+		Timeout: time.Second * 5, // Maximum of 5 secs
+	}
+
+	requester := IPInfoRequester{Client: client}
+	ctx := context.Background()
+	_, ipInfoErr := RetrieveIPInfoFromResponse(requester)
+	if ipInfoErr != nil {
+		t.Errorf("IPInfoRequester with real client should not fail, error was \"%s\".", ipInfoErr.Error())
+	} else {
+
+		if ipInfo.OrgName == "DIGI" {
+			t.Errorf("ipInfo.OrgName should not be DIGI.")
+		}
+	}
+}
